ca372add4e8c60dca5a0346c5dcabed9276615f0
vishal <vgouli1998@gmail.com>
2025-05-09 11:17:17 +0200

    chat bot changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,14 +14,14 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
+                Bonjour!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
               <h1 className="heading-name">
-                I'm
+                Je Suis
                 <strong className="main-name"> VISHAL GOULI</strong>
               </h1>
 
